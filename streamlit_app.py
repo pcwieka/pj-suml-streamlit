58aee1710a900dd2386689978734d56782becd82
pcwieka <paweljcwieka@gmail.com>
2024-05-13 20:10:02 +0200

    Complete implementation of Lab04

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,81 +1,52 @@
 import streamlit as st
-import pandas as pd
-import time
-import matplotlib as plt
-import os
-# zaczynamy od zaimportowania bibliotek
-
-st.success('Gratulacje! Z powodzeniem uruchomiłeś aplikację')
-# streamlit jest wykorzystywany do tworzenia aplikacji
-# z tego powodu dobrą praktyką jest informowanie użytkownika o postępie, błędach, etc.
-
-# Inne przykłady do wypróbowania:
-st.balloons() # animowane balony ;)
-# st.error('Błąd!') # wyświetla informację o błędzie
-# st.warning('Ostrzeżenie, działa, ale chyba tak sobie...')
-# st.info('Informacja...')
-# st.success('Udało się!')
-
-# st.spinner()
-# with st.spinner(text='Pracuję...'):
-    # time.sleep(2)
-    # st.success('Done')
-# możemy dzięki temu "ukryć" późniejsze ładowanie aplikacji
-
-st.title('Lab05. Streamlit')
-# title, jak sama nazwa wskazuje, używamy do wyświetlenia tytułu naszej aplikacji
-
-st.header('Wprowadzenie do zajęć')
-# header to jeden z podtytułów wykorzystywnaych w Streamlit
-
-st.subheader('O Streamlit')
-# subheader to jeden z podtytułów wykorzystywnaych w Streamlit
-
-st.text('To przykładowa aplikacja z wykorzystaniem Streamlit')
-# text używamy do wyświetlenia dowolnego tekstu. Można korzystać z polskich znaków.
-
-st.write('Streamlit jest biblioteką pozwalającą na uruchomienie modeli uczenia maszynowego.')
-# write używamy również do wyświetlenia tekstu, różnica polega na formatowaniu.
-
-st.code("st.write()", language='python')
-# code może nam się czasami przydać, jeżeli chcielibyśmy pokazać np. klientowi fragment kodu, który wykorzystujemy w aplikacji
-
-with st.echo():
-    st.write("Echo")
-# możemy też to zrobić prościej używając echo - pokazujemy kod i równocześnie go wykonujemy
-
-df = pd.read_csv("DSP_4.csv", sep = ';')
-st.dataframe(df)
-# musimy tylko pamiętać o właściwym określeniu separatora (w tym wypadku to średnik)
-# masz problem z otworzeniem pliku? sprawdź w jakim katalogu pracujesz i dodaj tam plik (albo co bardziej korzystne - zmień katalog pracy)
-# os.getcwd() # pokaż bieżący katalog
-# os.chdir("") # zmiana katalogu
-
-st.header('Przetwarzanie języka naturalnego')
-
-import streamlit as st
 from transformers import pipeline
 
+st.title('Polyglot')
+st.divider()
+st.image('sprechen_sie_deutsch.jpg')
+
+st.write('''
+Polyglot is designed to bridge language barriers and uncover the underlying emotions in texts.
+
+- **Text Translation**: Instantly convert English sentences into German with state-of-the-art accuracy.
+- **Sentiment Analysis**: Uncover the emotional tone of English texts, from joyful to sorrowful, with a single click.
+''')
+
+st.divider()
+
 option = st.selectbox(
-    "Opcje",
+    "Options",
     [
-        "Wydźwięk emocjonalny tekstu (eng)",
-        "???",
-    ],
+        "Text translation from English to German",
+        "Sentiment analysis of text (eng)",
+    ]
 )
 
-if option == "Wydźwięk emocjonalny tekstu (eng)":
-    text = st.text_area(label="Wpisz tekst")
+# Translating text from English to German
+if option == "Text translation from English to German":
+    text = st.text_area("Enter the text in English")
     if text:
-        classifier = pipeline("sentiment-analysis")
-        answer = classifier(text)
-        st.write(answer)
+        with st.spinner("Translating..."):
+            try:
+                translator = pipeline("translation", model="Helsinki-NLP/opus-mt-en-de")
+                translation = translator(text, max_length=512)
+                st.write('Translation to German:')
+                st.text(translation[0]['translation_text'])
+                st.balloons()
+            except Exception as e:
+                st.error(f"An error occurred during translation: {e}")
 
-st.subheader('Zadanie do wykonania')
-st.write('Wykorzystaj Huggin Face do stworzenia swojej własnej aplikacji tłumaczącej tekst z języka angielskiego na język niemiecki. Zmodyfikuj powyższy kod dodając do niego kolejną opcję, tj. tłumaczenie tekstu. Informacje potrzebne do zmodyfikowania kodu znajdziesz na stronie Huggin Face - https://huggingface.co/docs/transformers/index')
-st.write('🐞 Dodaj właściwy tytuł do swojej aplikacji, może jakieś grafiki?')
-st.write('🐞 Dodaj krótką instrukcję i napisz do czego służy aplikacja')
-st.write('🐞 Wpłyń na user experience, dodaj informacje o ładowaniu, sukcesie, błędzie, itd.')
-st.write('🐞 Na końcu umieść swój numer indeksu')
-st.write('🐞 Stwórz nowe repozytorium na GitHub, dodaj do niego swoją aplikację, plik z wymaganiami (requirements.txt)')
-st.write('🐞 Udostępnij stworzoną przez siebie aplikację (https://share.streamlit.io) a link prześlij do prowadzącego')
+# Sentiment analysis of the text
+if option == "Sentiment analysis of text (eng)":
+    text = st.text_area("Enter the text")
+    if text:
+        with st.spinner("Analyzing sentiment..."):
+            try:
+                classifier = pipeline("sentiment-analysis")
+                answer = classifier(text)
+                st.write("Sentiment analysis result:", answer)
+            except Exception as e:
+                st.error(f"An error occurred during sentiment analysis: {e}")
+
+st.divider()
+st.write('Created by s22000')
